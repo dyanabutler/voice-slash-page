21bc6d9acc2e955e0351513a5fc4ab42ac730d68
Dyana Butler <dyanajbutler98@gmail.com>
2024-07-06 14:39:32 -0400

    moved blur to the background

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,17 @@ import Reviews from "./components/Reviews";
 export default function Home() {
   return (
     <div className="bg-[#141414] dark:bg-[#F0F0F0] w-full min-h-screen relative">
-      <div className="fixed top-0 left-0 w-[300px] h-[300px] bg-[#2887C3] blur-[100px] opacity-50 dark:opacity-30" />
-      <div className="fixed bottom-0 right-0 w-[300px] h-[300px] bg-[#2887C3] blur-[100px] opacity-50 dark:opacity-30" />
-      <main className="container mx-auto px-4 py-12 md:py-24 lg:py-32">
+      
+      
+      <main className="container mx-auto px-4 py-12 md:py-24 lg:py-32 relative z-10">
         <Main />
         <Section2 />
         <Section3 />
         <Section4 />
         <Reviews />
       </main>
+      <div className="fixed top-0 left-0 w-[300px] h-[300px] bg-[#2887C3] blur-[100px] opacity-50 dark:opacity-30 z-0" />
+      <div className="fixed bottom-0 right-0 w-[300px] h-[300px] bg-[#2887C3] blur-[100px] opacity-50 dark:opacity-30 z-0" />
     </div>
   );
 }
